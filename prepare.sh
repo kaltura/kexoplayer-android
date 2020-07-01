241dcc0884f6217faf6cb209cc5d6d22abb54bf9
Gilad Nadav <gilad.nadav@kaltura.com>
2020-07-01 12:11:12 +0300

    avoid conflict with exo player ids FEC-10245

--- prepare.sh
+++ prepare.sh
@@ -37,5 +37,11 @@ find "$OUTPUT_DIR/lib/src/main" -type f \( -name "*.gradle" -o  -name "*.md" -o 
 # Add R import to UI source files
 find "$OUTPUT_DIR/lib/src/main/java/com/kaltura/android/exoplayer2/ui" -type f -name "*.java" -exec perl -i -p -e 's/package com\.kaltura\.android\.exoplayer2\.ui;/package com.kaltura.android.exoplayer2.ui;\n\nimport com.kaltura.android.exoplayer2.R;/' {} \;
 
+# Rename ids exo->kexo
+find . -type f -name "*.xml"  -exec perl -i -p -e 's/\@id\/exo_/\@id\/kexo_/' {} \;
+find . -type f -name "*.xml"  -exec perl -i -p -e 's/\@\+id\/exo_/\@\+id\/kexo_/' {} \;
+find . -type f -name "ids.xml"  -exec perl -i -p -e 's/name\=\"exo\_/name\=\"kexo\_/' {} \;
+find . -type f -name "*.java"  -exec perl -i -p -e 's/R\.id\.exo_/R\.id\.kexo_/' {} \;
+
 # Constants file
 cp "$INPUT_DIR/constants.gradle" "$OUTPUT_DIR/lib"
