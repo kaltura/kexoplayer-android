93aee419a6fef55f9ed186f44fce66b3cfe08ce2
Gilad Nadav <gilad.nadav@kaltura.com>
2020-10-06 22:51:34 +0300

    fix label in subtitles is missing for exo 2.12.0

--- dist.sh
+++ dist.sh
@@ -12,15 +12,15 @@ mkdir "$WORK_DIR"
 cd "$WORK_DIR"
 
 ## Step 1: Download ExoPlayer
-curl -L https://github.com/google/ExoPlayer/archive/r$VERSION.tar.gz | tar -xz
+curl -L https://github.com/giladna/ExoPlayer/archive/v2.12.0.1.zip | tar -xz
 
 ## Step 2: Prepare the sources
-$MYDIR/prepare.sh "ExoPlayer-r$VERSION" dist
+$MYDIR/prepare.sh "ExoPlayer-$VERSION" dist
 	
 ## Step 3: Build
 cd dist
 ./gradlew publishReleasePublicationToMavenLocal
 
 ## Step 4: Push to Bintray
-./gradlew bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
+#./gradlew bintrayUpload -PbintrayUser=$BINTRAY_USER -PbintrayKey=$BINTRAY_KEY -PdryRun=false
 	
