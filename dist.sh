0c4c80c68ef4f04d07e7dbd3e1e72bcf379a8d0d
Gilad Nadav <gilad.nadav@kaltura.com>
2023-06-12 00:55:00 +0300

    rollback

--- dist.sh
+++ dist.sh
@@ -20,5 +20,5 @@ $MYDIR/prepare.sh "ExoPlayer-r$VERSION" dist
 ## Step 3: Build
 ## Step 4: Push to Maven Central
 cd dist
-./gradlew --no-daemon build publishToSonatype
+./gradlew --no-daemon build publish
 #./gradlew build uploadArchives
